ed217d9ed3c059037039f15ffe6f4cbafcaa42b0
Ayushi Panwar <112766841+Ayushi024@users.noreply.github.com>
2023-08-30 14:30:24 +0530

    Add files via upload

---
+++ Resume Builder/script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+    $('.repeater').repeater({
+        initEmpty: false,
+        defaultValues: {
+            'text-input': ''
+        },
+        show:function(){
+            $(this).slideDown();
+        },
+        hide: function(deleteElement){
+            $(this).slideUp(deleteElement);
+            setTimeout(() => {
+                generateCV();
+            }, 500);
+        },
+        isFirstItemUndeletable: true
+    })
+})
